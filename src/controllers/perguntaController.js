fa4dff469ebbcfcffc8293fd77a4207b175e3220
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-05-13 00:20:18 -0300

    req tests

--- src/controllers/perguntaController.js
+++ src/controllers/perguntaController.js
@@ -2,6 +2,68 @@ import perguntas from "../models/Perguntas.js";
 
 class PerguntaController {
 
+    /*
+
+    static listarPerguntasPorParametro = (req, res) => {
+        
+        const { text } = req;
+        
+        if(text != null || text != undefined) {
+            let duvida = buscaDuvida(text);
+            perguntas.findOne({ duvidas:  })
+        } else {
+            res.status(400).send({ message: `${err.message} - id do item não encontrado.` })
+        }
+
+
+        const {id} = req.params;
+
+        perguntas.findById(id, (err, perguntas) => {
+            if(err){
+                res.status(400).send({ message: `${err.message} - id do item não encontrado.` })
+            } else { 
+
+                perguntas.forEach(objetos => {
+                    if(objetos.titulo == buscaDuvida(text)) {
+                        res.status(200).send(objetos.titulo);
+                    }
+                    
+                })
+
+                res.status(200).send(perguntas);
+            }
+        })
+
+
+            
+    function buscaDuvida(param, text){
+    
+        let vtDuvidasFP = duvidas.farma_popular;
+        let matches = [];
+    
+        let objResposta = [{erro: 'pergunta não encontrada'}];
+    
+        if(typeof text === 'number'){
+            matches = vtDuvidasFP[text];
+            if(matches) objResposta = [matches];
+        }else{
+            const perguntaFormatada = text.toLowerCase().removeAcentoECaracEspeciais();
+            const pgFormatada = duvida.pergunta.toLowerCase();
+            matches = vtDuvidas.filter(duvida => {return pgFormatada.indexOf(perguntaFormatada) !== -1});
+            if(matches.length > 0) {
+                objResposta = [];
+                for(match of matches) objResposta.push(match);
+            }
+        }
+    
+        return objResposta;
+    
+    }
+
+      
+
+    }     */
+
     static listarPerguntas = (req, res) => {
         perguntas.find((err, perguntas) => {
             res.status(200).json(perguntas)
