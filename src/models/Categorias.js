60f8a29ad16daaabd3034711f1c2ab3e3b7aed94
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-05-24 21:55:28 -0300

    doc schema categorias

--- src/models/Categorias.js
+++ src/models/Categorias.js
@@ -4,6 +4,7 @@ const categoriasSchema = new mongoose.Schema({
     id: { type: String },
     nome: { type: String, required: true },
     descricao: { type: String, required: true },
+    documentacao: { type: String, required: true },
     listaRemedios: [{ type: mongoose.Schema.Types.ObjectId, ref: 'remedios', required: true }]
 }, 
 {
